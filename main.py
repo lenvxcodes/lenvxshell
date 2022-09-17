43e3e4fbe025285118df9c4dbc179d61a0acc4d5
Lenvx <111296626+lenvxcodes@users.noreply.github.com>
2022-09-17 16:41:10 +0300

    added new commands, and datetime.

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Lev shell"""
-
+rst = 0
 
 kebab = False
 import os
 import readline
+import getpass
 LNX_sys = os.system
 from atexit import register as fn_register
+from datetime import date
 
 HISTORY_FILE: str = "sh_history"
 
@@ -27,11 +29,11 @@ import pkg_resources
 
 print("Welcome to startup..")
 print("Wellcome to DEV release,")
-print("I will check if u have installed the modules. else it wont work.")
+print("I will check if u have installed the modules. else it
 for package in ["colorama", "ujson"]:
     try:
         dist = pkg_resources.get_distribution(package)
-        print("{} ({}) Is installed!".format(dist.key, dist.version))
+        print("{} ({}) Is installed!".format(dist.key, dist.
     except pkg_resources.DistributionNotFound:
         print("{} Is not installed. OOF.".format(package))
         a = input("enter = exit()")
@@ -47,7 +49,7 @@ def reg():
     print("so. what password will u set here?")
     password = input("pass:")
     jsons = open("data/data.json", "w")
-    dict1 = {"user": username, "pass": password, "login": "True", "prefix": f"{use
+    dict1 = {"user": username, "pass": password, "login": "T
     ujson.dump(dict1, jsons, indent=6)
     print("just saved into json file.")
 
@@ -62,17 +64,34 @@ print("You are ready to go.")
 if config["login"] == "True":
     user = config["user"]
     prefix = config["prefix"]
-
+    def Confirmation(com):
+        print("Confirmation")
+        passconfirm =getpass.getpass(f"Pass for {user}, {com
+        if passconfirm == config["pass"]:
+            rst = 0
+            pass
+        if not passconfirm == config["pass"]:
+            print("Try again.")
+            if rst == 3:
+                print("INFl")
+                rst = 0
+                pass
+            rst = rst + 1
+            return Confirmation()
     def terminal():
         kebab = True
         print(f"Wellcome! {user}")
         print("enter your password.")
-        passchk = input("pass: ")
+        passchk = getpass.getpass("pass: ")
         if passchk == config["pass"]:
             os.system("clear")
             print("Wellcome back lenvx!")
+            today = date.today()
+            d1 = today.strftime("%d/%m/%Y")
+            print(d1)
+            print("------------------------------------")
             while kebab:
-                sytax: str = input(prefix.replace("{user}", config["user"]))
+                sytax: str = input(prefix.replace("{user}", 
                 if sytax == "help":
                     print(f"Commands for {user}:")
                     print("FNX(message)")
@@ -80,11 +99,12 @@ if config["login"] == "True":
                     print("resLNX >> restarts script.")
                     print("clrhy >> clears history")
                     print("credits")
+                    print("LNXkill >> exits the terminal.")
                 elif sytax == "CNGprefix":
                     print(f"current prefix:{prefix}")
                     prefixe = input("prefix:")
                     config["key"] = "value"
-                    with open("data/data.json", "w") as cfgf:
+                    with open("data/data.json", "w") as cfgf
                         ujson.dump(config, cfgf, indent=4)
                 elif sytax.startswith("LNX("):
                     print(sytax[4:-1])
@@ -93,12 +113,26 @@ if config["login"] == "True":
                     os.system("clear && python main.py")
                     return
                 elif sytax == "credits":
-                    print("very big credit to @TruncatedDinosour on github (ari)")
+                    print("very big credit to @TruncatedDino
                 elif sytax == "clrhy":
                     print("cleaned the history..")
                     his = open(HISTORY_FILE, "w").close()
                 elif not sytax.strip():
                     pass
+                if sytax == "LNXkill":
+                    print("Exiting...")
+                    exit()
+                if sytax == "LNXpass":
+                    Confirmation('LnxPass')
+                    
+                else:
+                    print("Oof.")
+        if not passchk == config["pass"]:
+            print("Console: Incorrect password.")
+            print("Reloading...")
+            return terminal(), LNX_sys('clear')
+terminal()
+exit()
                 else:
                     print("Oof.")
         if not passchk == config["pass"]:
